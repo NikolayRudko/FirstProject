0b7bb98df4294fb57bceea317dfb32249b7ee4b7
Rudko Nikolay <Niklaus.rud@gmail.com>
2019-07-15 14:44:53 +0300

    Update Task04 add recursion version, remote possibility enter negative numbers

--- src/Tasks/Task04.java
+++ src/Tasks/Task04.java
@@ -10,17 +10,15 @@ public class Task04 {
 
     private int inputNum() {
         int num;
-
         do {
-            System.out.print("Input number factorial not more 16: ");
+            System.out.print("Input positive integer number factorial not more 16: ");
             while (!in.hasNextInt()) {
                 System.out.println("That not a number!");
                 in.next(); // this is important!
                 System.out.print("Input number factorial not more 16: ");
             }
             num = in.nextInt();
-        } while (num > 16);
-
+        } while (num > 16 || num < 0);
         System.out.println(num);
         return num;
     }
@@ -28,20 +26,28 @@ public class Task04 {
     //Calculation of factorial
     public void factorialCalculation() {
         numberN = inputNum();
-        if (numberN < 0)                            // If the user entered a negative number
+        factorialN = calculationCycle(numberN);
+        System.out.printf("Factorial number %d = %d%n", numberN, factorialN);
+        factorialN = calculateRecursion(numberN);
+        System.out.printf("Factorial number %d = %d%n", numberN, factorialN);
+    }
+
+    private int calculationCycle(int num) {
+        int fact;
+        if (num == 0)                           // If the user entered a 0
+            return 1;                         // factorial 0 = 1
+        else // In all other cases
         {
-            factorialN = 0;                         // result 0
-            System.out.printf("Factorial number %d = %d", numberN, factorialN);
-        } else if (numberN == 0)                           // If the user entered a 0
-        {
-            factorialN = 1;                         // factorial 0 = 1
-            System.out.printf("Factorial number %d = %d", numberN, factorialN);
-        } else // In all other cases
-        {
-            factorialN = 1;
-            for (int i = 1; i < numberN; i++)
-                factorialN *= i;
-            System.out.printf("Factorial number %d = %d", numberN, factorialN);
+            fact = 1;
+            for (int i = 1; i <= num; i++)
+                fact *= i;
+            return fact;
         }
+    }
+
+    //Recursion version
+    private int calculateRecursion(int num) {
+        if (num == 0) return 1;
+        return calculateRecursion(num - 1) * num;
     }
 }
